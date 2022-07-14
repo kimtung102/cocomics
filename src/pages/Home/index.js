b0e9cfa7feb004ab6d8822a96048b34977729e59
NguyenNhuDang <108847798+NguyenNhuDang@users.noreply.github.com>
2022-07-14 00:54:42 -0700

    Update index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -16,42 +16,7 @@ import lg from './assets/Trang chủ/Login.png';
 
 function Home() {
   return (
-    //Thanh điều hướng ở đầu trang chủ - Nguyễn Như Đăng
-      <div className="Home">
-     <header className="App-top">
-      <Container 
-            
-            style={{height: 70,  backgroundImage: `url(${background})`, borderColor: 'transparent' }}>
-      <ButtonGroup aria-label="Basic example">
-      <Button variant="secondary" style={{backgroundImage: `url(${logo})`,justifyContent: 'center', width: 90 , height: 46, alignItems: 'center', borderColor: 'transparent' }} >
-      
-      </Button>
-      <img src= {TL} />
-      <Button variant="secondary" style ={{fontFamily: 'Mulish Bold', fontWeight: 'bold', borderColor: 'transparent'}}>
-        Thể loại
-      </Button>
-      <img src= {bxh} />
-      <Button variant="secondary" style ={{fontFamily: 'Mulish Bold', fontWeight: 'bold', borderColor: 'transparent'}}>
-        BXH
-      </Button>
-      <img src= {dang} />
-      <Button variant="secondary" style ={{fontFamily: 'Mulish Bold', fontWeight: 'bold', borderColor: 'transparent'}}>
-        Dang
-      </Button>
-      <TextField
-          id="outlined-basic"
-          variant="outlined"
-          label="Tìm kiếm"
-          style = {{width: 300, height: 20}}
-        />
-      <Button variant="secondary" style={{backgroundImage: `url(${lg})`,justifyContent: 'center', width: 135 , height: 38, alignItems: 'center', borderColor: 'transparent'}}>
-       
-      </Button>
-    </ButtonGroup>
     
-    </Container>
-     </header>
-    </div>
   );
 }
 
