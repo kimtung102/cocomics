8550c6b4bd9c6417f37e3140e8747da994daa09c
NguyenNhuDang <108847798+NguyenNhuDang@users.noreply.github.com>
2022-07-14 00:57:56 -0700

    Update index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,3 @@
-import './App.css';
-import btt from './features/home';
-import styled from 'styled-components';
-import React from 'react';
-import Navbar from './features/navbar';
-import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import Container from 'react-bootstrap/Container';
-import background from './assets/nvbar.png'
-import logo from './assets/Logo.png'
-import TextField from "@mui/material/TextField";
-import TL from './assets/Trang chủ/Tailieu.png';
-import bxh from './assets/Trang chủ/BXH.png';
-import dang from './assets/Trang chủ/Dang.png';
-import lg from './assets/Trang chủ/Login.png';
-
 function Home() {
   return (
     
