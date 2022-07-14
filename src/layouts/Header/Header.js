a79e7455775c8b14604c9a417e4cef56ea71483c
kimtung102 <tungnk20@gmail.com>
2022-07-14 17:30:23 +0700

    ..

--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -4,7 +4,11 @@ import styles from './Header.module.scss';
 const cx = className.bind(styles);
 
 function Header() {
-    return <div className={cx('wrapper')}>header</div>;
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('inner')}>inner</div>
+        </div>
+    );
 }
 
 export default Header;
