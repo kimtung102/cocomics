2c4959eedeb1faf9408916a18006e9b997662ea3
kimtung102 <tungnk20@gmail.com>
2022-07-26 02:46:03 +0700

    tach component

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -8,8 +8,7 @@ import Footer from '~/layouts/Footer/Footer';
 import MySlider from '~/components/Slider/MySlider';
 import SmallCard1 from '~/components/ComicCard/SmallCard1';
 import Button from '~/components/Button/Button';
-import imageSmall from '~/assets/images/hucau-small.svg';
-import imagelarge from '~/assets/images/hucau-large.svg';
+import LargeCard2 from '~/components/ComicCard/LargeCard2';
 
 const cx = className.bind(styles);
 
@@ -45,47 +44,7 @@ function Home() {
                     <div className={cx('funny-wrapper')}>
                         <div className={cx('left')}>
                             <p className={cx('title-no-border')}>Truyện hư cấu đang lên</p>
-                            <div className={cx('left-wrapper')}>
-                                <div className={cx('list-image')}>
-                                    <img
-                                        src={imageSmall}
-                                        alt="#"
-                                        width={'60px'}
-                                        style={{
-                                            border: '3px solid var(--primary)',
-                                            'border-radius': '6px',
-                                        }}
-                                    />
-                                    <img src={imageSmall} alt="#" width={'60px'} />
-                                    <img src={imageSmall} alt="#" width={'60px'} />
-                                    <img src={imageSmall} alt="#" width={'60px'} />
-                                    <img src={imageSmall} alt="#" width={'60px'} />
-                                    <img src={imageSmall} alt="#" width={'60px'} />
-                                    <img src={imageSmall} alt="#" width={'60px'} />
-                                    <img src={imageSmall} alt="#" width={'60px'} />
-                                </div>
-                                <div className={cx('content-wrapper')}>
-                                    <span className={cx('main-image')}>
-                                        <img src={imagelarge} alt="#" width={'124px'} />
-                                    </span>
-                                    <div className={cx('card-body')}>
-                                        <a href="/" className={cx('content-title')}>
-                                            Kinh nghiệm cảnh nóng
-                                        </a>
-                                        <a href="/" className={cx('content-category')}>
-                                            Boy Love
-                                        </a>
-                                        <span className={cx('content-summary')}>
-                                            Tae Gyum sau khi phát hiện mình là một Alpha trội gặp tai họa khi vô tình
-                                            khiến một Omega phát tình, cậu trở nên né tránh là một Alpha trội gặp tai
-                                            họa khi vô tình khiến một Omega phát tình, cậu trở nên né tránh
-                                        </span>
-                                        <Button primary small>
-                                            Đọc ngay
-                                        </Button>
-                                    </div>
-                                </div>
-                            </div>
+                            <LargeCard2 />
                         </div>
                         <div className={cx('right')}>
                             <p className={cx('title')}>Truyện vui</p>
