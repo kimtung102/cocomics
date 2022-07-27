cc87821a8f1782ac00b0c93c80910dea395840f0
kimtung102 <tungnk20@gmail.com>
2022-07-28 05:19:26 +0700

    user - popover

--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -1,6 +1,7 @@
 import className from 'classnames/bind';
 import styles from './Header.module.scss';
 import logo from '~/assets/images/logo.svg';
+import avatar from '~/assets/images/comic-image.svg';
 import categoryIcon from '~/assets/images/category-icon.svg';
 import bxhIcon from '~/assets/images/bxh-icon.svg';
 import Button from '~/components/Button/Button';
@@ -65,14 +66,41 @@ function Header() {
                     </div>
                     <Search />
                 </div>
-                <div className={cx('right-block')}>
-                    <Button primary onClick={handleLoginShow}>
-                        Đăng nhập
-                    </Button>
-                    <Button rounded onClick={handleSignInShow}>
-                        Đăng ký
-                    </Button>
-                </div>
+                {currentUser ? (
+                    <Tippy
+                        interactive
+                        placement="bottom-end"
+                        render={(attrs) => (
+                            <div className={cx('user-popover')} tabIndex="-1" {...attrs}>
+                                <div className={cx('info')}>
+                                    <img src={avatar} alt="nguyen van a" className={cx('user-avatar')}></img>
+                                    <div className={cx('info-text')}>
+                                        <p className={cx('username')}>thedc123233</p>
+                                        <p className={cx('id')}>ID: 12213001023</p>
+                                    </div>
+                                </div>
+                                <p className={cx('option')}>
+                                    <a href="/">Thông tin cá nhân</a>
+                                    <a href="/">Yêu thích</a>
+                                    <a href="/">Lịch sử đọc</a>
+                                    <a href="/">Cài đặt</a>
+                                    <a href="/">Đăng xuất</a>
+                                </p>
+                            </div>
+                        )}
+                    >
+                        <img src={avatar} alt="nguyen van a" className={cx('user-avatar')}></img>
+                    </Tippy>
+                ) : (
+                    <div className={cx('right-block')}>
+                        <Button primary onClick={handleLoginShow}>
+                            Đăng nhập
+                        </Button>
+                        <Button rounded onClick={handleSignInShow}>
+                            Đăng ký
+                        </Button>
+                    </div>
+                )}
             </div>
             <Modal popup={popup} />
         </div>
