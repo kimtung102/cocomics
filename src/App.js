be2545770081091ebac0c640430a483230759102
kimtung102 <tungnk20@gmail.com>
2022-08-04 02:34:31 +0700

    done user page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from '~/pages/HomePage/Home';
 import Ranking from '~/pages/Ranking/Ranking';
+import UserPage from './pages/UserPage/UserPage';
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/ranking" element={<Ranking />} />
+                    <Route path="/user" element={<UserPage />} />
                 </Routes>
             </div>
         </Router>
